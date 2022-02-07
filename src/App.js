e172e38b309b02eba3d71ec4e5ea3ef83b85b06a
Drew Hollis <drewj1031@gmail.com>
2022-02-07 12:50:12 -0600

    removed line chart due to undefined new Date object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import {
   Navbar,
